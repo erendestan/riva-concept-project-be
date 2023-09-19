386c029aa0a7e92f8abc5fc9536b9d89dc4c400e
Eren Destan <r0889641@student.thomasmore.be>
2023-09-19 22:13:26 +0200

    Start up files

---
+++ src/main/java/com/example/rivaconceptproject/RivaConceptProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.rivaconceptproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RivaConceptProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RivaConceptProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rivaconceptproject/RivaConceptProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rivaconceptproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RivaConceptProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
