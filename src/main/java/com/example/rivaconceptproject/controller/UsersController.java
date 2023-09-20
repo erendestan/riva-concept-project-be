5b1c2395766c0cf7367f5b82ea8fd27137859cd9
Eren Destan <e.destan@student.fontys.nl>
2023-09-20 15:23:37 +0200

    UserController has been created but no codes inside

---
+++ src/main/java/com/example/rivaconceptproject/controller/UsersController.java
@@ -0,0 +1,10 @@
+package com.example.rivaconceptproject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //Means Endpoint is defined here
+@RequestMapping("/users") //And endpoint is connected to /users
+public class UsersController {
+
+}
