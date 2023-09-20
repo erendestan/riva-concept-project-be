7107dddf0bfcd67d254f2dd1fcc5b877d51a4b75
Eren Destan <e.destan@student.fontys.nl>
2023-09-20 11:06:33 +0200

    User domain and Role enum has been added

---
+++ src/main/java/com/example/rivaconceptproject/domain/User.java
@@ -0,0 +1,20 @@
+package com.example.rivaconceptproject.domain;
+
+import com.example.rivaconceptproject.domain.enums.Role;
+
+import java.util.Date;
+
+public class User {
+    private Long id;
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+    private Long phoneNumber;
+
+    private Date birthDate;
+
+    private Role role;
+
+}
---
+++ src/main/java/com/example/rivaconceptproject/domain/enums/Role.java
@@ -0,0 +1,7 @@
+package com.example.rivaconceptproject.domain.enums;
+
+public enum Role {
+    Customer,
+    Worker,
+    Admin
+}
