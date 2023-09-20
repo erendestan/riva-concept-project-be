54a9f1b89b4a3f54d01a95a993cfc2772759e53f
Eren Destan <e.destan@student.fontys.nl>
2023-09-20 14:41:38 +0200

    UserRepository has been added

--- src/main/java/com/example/rivaconceptproject/persistence/UserRepository.java
+++ src/main/java/com/example/rivaconceptproject/persistence/UserRepository.java
@@ -1,4 +1,19 @@
 package com.example.rivaconceptproject.persistence;
 
+import com.example.rivaconceptproject.persistence.entity.UserEntity;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository {
+
+    boolean existsByUserId(long userId);
+
+    List<UserEntity> findAllUsers();
+
+    UserEntity save(UserEntity user);
+
+    void deleteById(long userId);
+
+    Optional<UserEntity> findUserById(long userId);
 }
