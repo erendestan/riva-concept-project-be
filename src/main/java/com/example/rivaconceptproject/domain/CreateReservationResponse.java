dd663de3a02db311886c213a8e90fefa14ded30f
Eren Destan <e.destan@student.fontys.nl>
2023-10-02 13:00:30 +0200

    CreateReservationResponse has been added

---
+++ src/main/java/com/example/rivaconceptproject/domain/CreateReservationResponse.java
@@ -0,0 +1,17 @@
+package com.example.rivaconceptproject.domain;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Data
+@Builder
+public class CreateReservationResponse {
+    private long reservationId;
+    private long userId;
+    private LocalDateTime reservationDate;
+    private LocalTime startTime;
+    private LocalTime endTime;
+}
