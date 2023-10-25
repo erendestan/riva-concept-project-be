c12ececc5c5e0d8e924bf4a28d9fecf3947f6d52
Eren Destan <e.destan@student.fontys.nl>
2023-10-25 12:46:05 +0200

    UpdateReservationRequest have been added.

---
+++ src/main/java/com/example/rivaconceptproject/domain/Reservation/UpdateReservationRequest.java
@@ -0,0 +1,32 @@
+package com.example.rivaconceptproject.domain.Reservation;
+
+import com.example.rivaconceptproject.domain.enums.Event;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateReservationRequest {
+    @NotNull
+    private long reservationId;
+    @NotNull
+    private long userId;
+    @NotNull
+    private Event eventType;
+    @NotNull
+    private LocalDateTime reservationCreatedDate;
+    @NotNull
+    private LocalDateTime reservationDate;
+    @NotNull
+    private LocalTime startTime;
+    @NotNull
+    private LocalTime endTime;
+}
