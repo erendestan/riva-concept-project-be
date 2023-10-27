21e4d36fab5ac3c6537ff3443d23c253664cd6b0
Eren Destan <e.destan@student.fontys.nl>
2023-10-27 11:36:54 +0200

    DeleteReservationUseCaseImpl have been finished.

--- src/main/java/com/example/rivaconceptproject/business/impl/ReservationUseCaseImpls/DeleteReservationUseCaseImpl.java
+++ src/main/java/com/example/rivaconceptproject/business/impl/ReservationUseCaseImpls/DeleteReservationUseCaseImpl.java
@@ -1,10 +1,16 @@
 package com.example.rivaconceptproject.business.impl.ReservationUseCaseImpls;
 
 import com.example.rivaconceptproject.business.ReservationUseCases.DeleteReservationUseCase;
+import com.example.rivaconceptproject.persistence.ReservationRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
 public class DeleteReservationUseCaseImpl implements DeleteReservationUseCase {
+    private final ReservationRepository reservationRepository;
     @Override
     public void deleteReservation(long reservationId) {
-
+        this.reservationRepository.deleteByReservationId(reservationId);
     }
 }
