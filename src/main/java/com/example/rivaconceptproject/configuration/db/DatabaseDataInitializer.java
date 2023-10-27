3f038c1545ad1805b670accfd1fcf35402ba6edf
Eren Destan <e.destan@student.fontys.nl>
2023-10-27 10:48:38 +0200

    DatabaseDataInitializer has been updated for reservations as well

--- src/main/java/com/example/rivaconceptproject/configuration/db/DatabaseDataInitializer.java
+++ src/main/java/com/example/rivaconceptproject/configuration/db/DatabaseDataInitializer.java
@@ -1,17 +1,25 @@
 package com.example.rivaconceptproject.configuration.db;
 
+import com.example.rivaconceptproject.domain.enums.Event;
 import com.example.rivaconceptproject.domain.enums.Role;
+import com.example.rivaconceptproject.persistence.ReservationRepository;
 import com.example.rivaconceptproject.persistence.UserRepository;
+import com.example.rivaconceptproject.persistence.entity.ReservationEntity;
 import com.example.rivaconceptproject.persistence.entity.UserEntity;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+
 @Component
 @AllArgsConstructor
 public class DatabaseDataInitializer {
     private UserRepository userRepository;
+    private ReservationRepository reservationRepository;
 
     @EventListener(ApplicationReadyEvent.class)  //When the application is runned this will be executed.
     public void populateDatabaseInitialDummyData() {
@@ -23,6 +31,16 @@ public class DatabaseDataInitializer {
                     .phoneNumber(31684469019L)
                     .password("123456eren")
                     .role(Role.Admin)
+                    .build());
+
+        System.out.println("***** Reservation has been added to system *****");
+            reservationRepository.save(ReservationEntity.builder()
+                    .userId(1)
+                    .eventType(Event.Wedding)
+                    .reservationCreatedDate(LocalDateTime.of(2023, 10, 27, 10, 33, 0))
+                    .reservationDate(LocalDateTime.of(2024, 6, 1, 18, 0, 0))
+                    .startTime(LocalTime.of(18, 0, 0))
+                    .endTime(LocalTime.of(23, 0, 0))
                     .build());
 
     }
