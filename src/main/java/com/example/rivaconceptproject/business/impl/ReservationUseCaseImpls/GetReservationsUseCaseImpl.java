30bfaac483e3697f53dd960ebd0ab726656a65be
Eren Destan <e.destan@student.fontys.nl>
2023-10-27 11:39:57 +0200

    GetReservationsUseCaseImpl have been finished.

--- src/main/java/com/example/rivaconceptproject/business/impl/ReservationUseCaseImpls/GetReservationsUseCaseImpl.java
+++ src/main/java/com/example/rivaconceptproject/business/impl/ReservationUseCaseImpls/GetReservationsUseCaseImpl.java
@@ -2,10 +2,26 @@ package com.example.rivaconceptproject.business.impl.ReservationUseCaseImpls;
 
 import com.example.rivaconceptproject.business.ReservationUseCases.GetReservationsUseCase;
 import com.example.rivaconceptproject.domain.Reservation.GetAllReservationsResponse;
+import com.example.rivaconceptproject.domain.Reservation.Reservation;
+import com.example.rivaconceptproject.persistence.ReservationRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class GetReservationsUseCaseImpl implements GetReservationsUseCase {
+    private final ReservationRepository reservationRepository;
     @Override
     public GetAllReservationsResponse getReservations() {
-        return null;
+        List<Reservation> reservations = reservationRepository.findAllReservations()
+                .stream()
+                .map(ReservationConverter::convert)
+                .toList();
+
+        return GetAllReservationsResponse.builder()
+                .reservations(reservations)
+                .build();
     }
 }
