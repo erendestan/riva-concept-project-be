e2809c33babc78c67ab4b27c211688e01229e139
Eren Destan <e.destan@student.fontys.nl>
2023-11-08 12:19:25 +0100

    DeleteUserUseCaseImplTest has been modified with Mockito and works fine.

--- src/test/java/com/example/rivaconceptproject/business/impl/DeleteUserUseCaseImplTest.java
+++ src/test/java/com/example/rivaconceptproject/business/impl/DeleteUserUseCaseImplTest.java
@@ -1,55 +1,48 @@
 package com.example.rivaconceptproject.business.impl;
 
-import com.example.rivaconceptproject.business.UserUseCases.DeleteUserUseCase;
+import com.example.rivaconceptproject.business.impl.UserUseCaseImpls.DeleteUserUseCaseImpl;
 import com.example.rivaconceptproject.domain.enums.Role;
 import com.example.rivaconceptproject.persistence.UserRepository;
 import com.example.rivaconceptproject.persistence.entity.UserEntity;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class DeleteUserUseCaseImplTest {
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    DeleteUserUseCase deleteUserUseCase;
+    @Mock
+    UserRepository userRepositoryMock;
+    @InjectMocks
+    DeleteUserUseCaseImpl deleteUserUseCase;
 
-    @BeforeEach
-    void settingUp(){
-//        userRepository.clear();
-//        userRepository = new FakeUserRepositoryImpl();
-        userRepository.save(UserEntity.builder()
+    @Test
+    void deleteUser_deletesByTheUserId(){
+        userRepositoryMock.save(UserEntity.builder()
+                .id(1)
                 .firstName("Jack")
                 .lastName("Kral")
                 .email("jackkral@gmail.com")
                 .phoneNumber("555666444")
                 .role(Role.Customer)
                 .build());
-        userRepository.save(UserEntity.builder()
+        userRepositoryMock.save(UserEntity.builder()
+                .id(2)
                 .firstName("Edward")
                 .lastName("Ox")
                 .email("edwardox@gmail.com")
                 .phoneNumber("666555111")
                 .role(Role.Worker)
                 .build());
-    }
-    @AfterEach
-    public void tearDown(){
-//        userRepository.clear();
-    }
 
-    @Test
-    void deleteUser_deletesByTheUserId(){
-//        DeleteUserUseCase deleteUserUseCase = new DeleteUserUseCaseImpl(userRepository);
+
         deleteUserUseCase.deleteUser(2);
-        assertFalse(userRepository.findById(2).isPresent());
-        assertEquals(1, userRepository.findAll().size());
+        assertFalse(userRepositoryMock.findById(2).isPresent());
+
+//        assertEquals(1, userRepositoryMock.findAll().size());
     }
 
 }
