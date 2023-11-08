04dd462336504adf968e8e2913dcd80295cfdc10
Eren Destan <e.destan@student.fontys.nl>
2023-11-08 11:45:53 +0100

    GetUsersUseCaseImplTest has been modified with Mockito and works fine.

--- src/test/java/com/example/rivaconceptproject/business/impl/GetUsersUseCaseImplTest.java
+++ src/test/java/com/example/rivaconceptproject/business/impl/GetUsersUseCaseImplTest.java
@@ -1,78 +1,111 @@
 package com.example.rivaconceptproject.business.impl;
 
-import com.example.rivaconceptproject.business.UserUseCases.GetUsersUseCase;
+import com.example.rivaconceptproject.business.impl.UserUseCaseImpls.GetUsersUseCaseImpl;
 import com.example.rivaconceptproject.domain.User.GetAllUsersResponse;
 import com.example.rivaconceptproject.domain.User.User;
 import com.example.rivaconceptproject.domain.enums.Role;
 import com.example.rivaconceptproject.persistence.UserRepository;
 import com.example.rivaconceptproject.persistence.entity.UserEntity;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
 
-@SpringBootTest
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class GetUsersUseCaseImplTest {
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    GetUsersUseCase getUsersUseCase;
-
-    @BeforeEach
-    void settingUp(){
-//        userRepository.clear();
-//        userRepository = new FakeUserRepositoryImpl();
-
-        userRepository.save(UserEntity.builder()
-                .firstName("Jack")
-                .lastName("Kral")
-                .email("jackkral@gmail.com")
-                .phoneNumber("555666444")
-                .role(Role.Customer)
-                .build());
-        userRepository.save(UserEntity.builder()
-                .firstName("Edward")
-                .lastName("Ox")
-                .email("edwardox@gmail.com")
-                .phoneNumber("666555111")
-                .role(Role.Worker)
-                .build());
-    }
-//    @AfterEach
-//    public void tearDown(){
-//        userRepository.clear();
-//    }
+    @Mock
+    UserRepository userRepositoryMock;
+    @InjectMocks
+    GetUsersUseCaseImpl getUsersUseCase;
 
     @Test
-    void getProducts_shouldGetAllTheProducts(){
-//        getUsersUseCase = new GetUsersUseCaseImpl(userRepository);
+    void getUsers_shouldGetAllTheUsers_ShouldReturnTheExpectedSize(){
+        when(userRepositoryMock.findAll()).thenReturn(
+                Arrays.asList(
+                        UserEntity.builder()
+                                .id(1)
+                                .firstName("Jack")
+                                .lastName("Kral")
+                                .email("jackkral@gmail.com")
+                                .phoneNumber("555666444")
+                                .role(Role.Customer)
+                                .build(),
+                        UserEntity.builder()
+                                .id(2)
+                                .firstName("Edward")
+                                .lastName("Ox")
+                                .email("edwardox@gmail.com")
+                                .phoneNumber("666555111")
+                                .role(Role.Worker)
+                                .build()
+                )
+        );
+        // Call the getUsers method
         GetAllUsersResponse result = getUsersUseCase.getUsers();
+
+        // Write assertions to validate the result
         assertEquals(2, result.getUsers().size());
         assertNotNull(result);
+        verify(userRepositoryMock).findAll();
+    }
 
-        User expectedUser_1 = User.builder()
-                .id(2)
+    @Test
+    void getUsers_shouldGetAllTheUsers_ShouldMatchTheExpectedDetails() {
+        // Define the expected UserEntity objects
+        UserEntity expectedUser1 = UserEntity.builder()
+                .id(1)
                 .firstName("Jack")
                 .lastName("Kral")
                 .email("jackkral@gmail.com")
                 .phoneNumber("555666444")
                 .role(Role.Customer)
                 .build();
-        User expectedUser_2 = User.builder()
-                .id(3)
+
+        UserEntity expectedUser2 = UserEntity.builder()
+                .id(2)
                 .firstName("Edward")
                 .lastName("Ox")
                 .email("edwardox@gmail.com")
                 .phoneNumber("666555111")
                 .role(Role.Worker)
                 .build();
-//        List<User> userList = new ArrayList<>();
-//        userList.add(expectedUser_1);
-//        userList.add(expectedUser_2);
-        assertTrue(result.getUsers().contains(expectedUser_1));
-        assertTrue(result.getUsers().contains(expectedUser_2));
+
+        // Stub the userRepositoryMock's behavior
+        when(userRepositoryMock.findAll()).thenReturn(Arrays.asList(expectedUser1, expectedUser2));
+
+        // Call the getUsers method
+        GetAllUsersResponse result = getUsersUseCase.getUsers();
+
+        User actualUser1 = User.builder()
+                .id(1)
+                .firstName("Jack")
+                .lastName("Kral")
+                .email("jackkral@gmail.com")
+                .phoneNumber("555666444")
+                .role(Role.Customer)
+                .build();
+
+        User actualUser2 = User.builder()
+                .id(2)
+                .firstName("Edward")
+                .lastName("Ox")
+                .email("edwardox@gmail.com")
+                .phoneNumber("666555111")
+                .role(Role.Worker)
+                .build();
+
+
+        // Verify that the returned UserEntity objects match the expected details
+        assertEquals(actualUser1, result.getUsers().get(0));
+        assertEquals(actualUser2, result.getUsers().get(1));
     }
 
 }
