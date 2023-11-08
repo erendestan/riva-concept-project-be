f35959e2ef130b1549f2eac3de91f9084202a689
Eren Destan <e.destan@student.fontys.nl>
2023-11-08 12:29:28 +0100

    GetUserUseCaseImplTest has been modified with Mockito and works fine.

--- src/test/java/com/example/rivaconceptproject/business/impl/GetUserUseCaseImplTest.java
+++ src/test/java/com/example/rivaconceptproject/business/impl/GetUserUseCaseImplTest.java
@@ -1,56 +1,66 @@
 package com.example.rivaconceptproject.business.impl;
 
-import com.example.rivaconceptproject.business.UserUseCases.GetUserUseCase;
+import com.example.rivaconceptproject.business.impl.UserUseCaseImpls.GetUserUseCaseImpl;
 import com.example.rivaconceptproject.domain.User.User;
 import com.example.rivaconceptproject.domain.enums.Role;
 import com.example.rivaconceptproject.persistence.UserRepository;
 import com.example.rivaconceptproject.persistence.entity.UserEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class GetUserUseCaseImplTest {
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    GetUserUseCase getUserUseCase;
+    @Mock
+    UserRepository userRepositoryMock;
+    @InjectMocks
+    GetUserUseCaseImpl getUserUseCase;
 
 
     @BeforeEach
     void settingUp(){
 //        userRepository.clear();
 
-        userRepository.save(UserEntity.builder()
+        UserEntity savedUser = UserEntity.builder()
+                .id(1)
                 .firstName("Jack")
                 .lastName("Kral")
                 .email("jackkral@gmail.com")
                 .phoneNumber("555666444")
                 .role(Role.Customer)
-                .build());
+                .build();
+        when(userRepositoryMock.findById(1)).thenReturn(Optional.of(savedUser));
     }
 
     @Test
     void getUserById_shouldGetTheUserByGivenId(){
-        UserEntity savedUser = userRepository.findAll().get(0);
-        long userId = savedUser.getId();
-
+        long userId = 1;
         Optional<User> userOptional = getUserUseCase.getUser(userId);
+
+//        UserEntity savedUser = userRepository.findAll().get(0);
+//        long userId = savedUser.getId();
+
+
 
         assertTrue(userOptional.isPresent());
         User user = userOptional.get();
         assertEquals("Jack", user.getFirstName());
         assertEquals("Kral", user.getLastName());
         assertEquals("jackkral@gmail.com", user.getEmail());
-        assertEquals(555666444, user.getPhoneNumber());
+        assertEquals("555666444", user.getPhoneNumber());
         assertEquals(Role.Customer, user.getRole());
 
+        verify(userRepositoryMock).findById(userId);
     }
 
 
