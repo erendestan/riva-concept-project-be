60e0410b4a152da502c935d6314324e34fa8db3e
Eren Destan <e.destan@student.fontys.nl>
2023-12-01 13:00:11 +0100

    some changes

--- src/test/java/com/example/rivaconceptproject/business/impl/ReservationUseCaseImplTests/UpdateReservationUseCaseImplTest.java
+++ src/test/java/com/example/rivaconceptproject/business/impl/ReservationUseCaseImplTests/UpdateReservationUseCaseImplTest.java
@@ -9,7 +9,7 @@ import com.example.rivaconceptproject.persistence.ReservationRepository;
 import com.example.rivaconceptproject.persistence.UserRepository;
 import com.example.rivaconceptproject.persistence.entity.ReservationEntity;
 import com.example.rivaconceptproject.persistence.entity.UserEntity;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -23,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class UpdateReservationUseCaseImplTest {
+class UpdateReservationUseCaseImplTest {
     @Mock
     private ReservationRepository reservationRepositoryMock;
     @Mock
@@ -33,7 +33,7 @@ public class UpdateReservationUseCaseImplTest {
     private UpdateReservationUseCaseImpl updateReservationUseCase;
 
     @Test
-    public void testUpdateReservation() {
+    void testUpdateReservation() {
         // Arrange
         UpdateReservationRequest request = createSampleUpdateReservationRequest();
         ReservationEntity existingReservation = createSampleReservationEntity();
@@ -49,8 +49,9 @@ public class UpdateReservationUseCaseImplTest {
         verify(userRepositoryMock).findById(request.getUserId());
         verify(reservationRepositoryMock).save(existingReservation); // Ensure that the save method is called
     }
+
     @Test
-    public void testUpdateReservationInvalidReservationId() {
+    void testUpdateReservationInvalidReservationId() {
         // Arrange
         UpdateReservationRequest request = createSampleUpdateReservationRequest();
         when(reservationRepositoryMock.findByReservationId(request.getReservationId())).thenReturn(Optional.empty());
@@ -64,7 +65,7 @@ public class UpdateReservationUseCaseImplTest {
     }
 
     @Test
-    public void testUpdateReservationInvalidUser() {
+    void testUpdateReservationInvalidUser() {
         // Arrange
         UpdateReservationRequest request = createSampleUpdateReservationRequest();
         ReservationEntity existingReservation = createSampleReservationEntity();
@@ -93,6 +94,7 @@ public class UpdateReservationUseCaseImplTest {
                 .endTime(LocalTime.of(12, 0))
                 .build();
     }
+
     private ReservationEntity createSampleReservationEntity() {
         return ReservationEntity.builder()
                 .reservationId(1L)
@@ -116,70 +118,4 @@ public class UpdateReservationUseCaseImplTest {
                 .role(Role.CUSTOMER)
                 .build();
     }
-//    @Mock
-//    ReservationRepository reservationRepositoryMock;
-//    @Mock
-//    UserRepository userRepositoryMock;
-//    @InjectMocks
-//    UpdateReservationUseCaseImpl updateReservationUseCase;
-//
-//    @BeforeEach
-//    void setUp() {
-//        UserEntity savedUser = UserEntity.builder()
-//                .id(1L)
-//                .firstName("Jack")
-//                .lastName("Kral")
-//                .email("jackkral@gmail.com")
-//                .phoneNumber("555444111")
-//                .password("testpassword")
-//                .role(Role.CUSTOMER)
-//                .build();
-//
-//        ReservationEntity savedReservation = ReservationEntity.builder()
-//                .reservationId(1L)
-//                .user(savedUser)
-//                .eventType(Event.WEDDING)
-//                .reservationCreatedDate(LocalDateTime.of(2022, 11, 15, 18, 0))
-//                .reservationDate(LocalDateTime.of(2023, 11, 15, 18, 0))
-//                .startTime(LocalTime.of(18, 0))
-//                .endTime(LocalTime.of(23, 0))
-//                .build();
-//
-//        when(reservationRepositoryMock.findByReservationId(1L)).thenReturn(Optional.of(savedReservation));
-//
-//        when(reservationRepositoryMock.findByReservationId(2L)).thenReturn(Optional.empty());
-//    }
-//
-//
-//        @Test
-//        void updateReservationShouldUpdateReservation() {
-//            UpdateReservationRequest updateReservationRequest = new UpdateReservationRequest();
-//            updateReservationRequest.setReservationId(1L);
-//            updateReservationRequest.setEventType(Event.OTHER);
-//            updateReservationRequest.setReservationCreatedDate(LocalDateTime.of(2021, 11, 15, 18, 0));
-//            updateReservationRequest.setReservationDate(LocalDateTime.of(2024, 11, 15, 18, 0));
-//            updateReservationRequest.setStartTime(LocalTime.of(20, 0));
-//            updateReservationRequest.setEndTime(LocalTime.of(0, 0));
-//
-//            updateReservationUseCase.updateReservation(updateReservationRequest);
-//
-//            // Verify that the repository.save method is called once with the correct argument
-//            verify(reservationRepositoryMock, times(1)).save(any());
-//
-//            // Optionally, you can use ArgumentCaptor to capture the actualReservation
-//            ArgumentCaptor<ReservationEntity> reservationCaptor = ArgumentCaptor.forClass(ReservationEntity.class);
-//            verify(reservationRepositoryMock).save(reservationCaptor.capture());
-//
-//            // Now, you can perform assertions on the captured reservation if needed
-//            ReservationEntity capturedReservation = reservationCaptor.getValue();
-//            assertNotNull(capturedReservation);
-//
-//            // Additional assertions...
-//            assertEquals(Event.OTHER, capturedReservation.getEventType());
-//            assertEquals(LocalDateTime.of(2021, 11, 15, 18, 0), capturedReservation.getReservationCreatedDate());
-//            assertEquals(LocalDateTime.of(2024, 11, 15, 18, 0), capturedReservation.getReservationDate());
-//            assertEquals(LocalTime.of(20, 0), capturedReservation.getStartTime());
-//            assertEquals(LocalTime.of(0, 0), capturedReservation.getEndTime());
-//    }
-
 }
